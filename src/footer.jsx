f921f274305a274df7f7738ac339f8eccbab0041
Noah Manneschmidt <noah@manneschmidt.net>
2018-03-15 20:47:01 -0700

    Add Koishigure to songs

--- src/footer.jsx
+++ src/footer.jsx
@@ -5,7 +5,7 @@ import globalStyles from './app.css';
 export function Footer(props) {
   return (
     <div className={classNames(styles.footer, globalStyles.padded)}>
-      By Jeff Lloyd and Noah Manneschmidt. Songs up to date as of 03/11/2018. <a href="https://github.com/noahm/DDRCardDraw">View on GitHub</a>
+      By Jeff Lloyd and Noah Manneschmidt. Songs up to date as of 03/15/2018. <a href="https://github.com/noahm/DDRCardDraw">View on GitHub</a>
     </div>
   );
 }
