9f3e5f8c0b07275833dac5def9370923d2ebebed
Noah Manneschmidt <noah@manneschmidt.net>
2019-03-28 02:06:42 -0700

    Make the footer better with the power of icons

--- src/footer.jsx
+++ src/footer.jsx
@@ -2,11 +2,28 @@ import classNames from 'classnames';
 import styles from './footer.css';
 import globalStyles from './app.css';
 
+import fbLogo from 'ionicons/dist/ionicons/svg/logo-facebook.svg';
+import twitterLogo from 'ionicons/dist/ionicons/svg/logo-twitter.svg';
+import githubLogo from 'ionicons/dist/ionicons/svg/logo-github.svg';
+
+function Icon({ src, title }) {
+  return (
+    <figure title={title} alt={title} dangerouslySetInnerHTML={{ __html: src }} className={styles.icon} />
+  );
+}
+
 export function Footer() {
   return (
-    <div className={classNames(styles.footer, globalStyles.padded)}>
-      Songs up to date as of 1/08/2019. Report bugs via <a href="https://twitter.com/Cathadan" target="_blank">Twitter</a> or <a href="https://github.com/noahm/DDRCardDraw">GitHub</a>. <a href="#" onClick={showCredits}>Credits.</a>
-    </div>
+    <footer className={classNames(styles.footer, globalStyles.padded)}>
+      <div>Songs up to date as of 1/08/2019. <a href="#" onClick={showCredits}>Credits</a></div>
+      <div>
+        <a href="https://m.me/noah.manneschmidt" target="_blank"><Icon src={fbLogo} title="Contact me on Facebook Messenger" /></a>
+        &nbsp;
+        <a href="https://twitter.com/Cathadan" target="_blank"><Icon src={twitterLogo} title="Contact me on Twitter" /></a>
+        &nbsp;
+        <a href="https://github.com/noahm/DDRCardDraw"><Icon src={githubLogo} title="Contribute on Github" /></a>
+      </div>
+    </footer>
   );
 }
 
