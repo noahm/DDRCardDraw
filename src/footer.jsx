e3653e75b58a15b62b8ea608c87b367df0b4a747
Noah Manneschmidt <noah@manneschmidt.net>
2019-08-15 23:00:25 -0700

    Bump last-updated date

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,27 +1,43 @@
-import classNames from 'classnames';
-import styles from './footer.css';
-import globalStyles from './app.css';
+import classNames from "classnames";
+import styles from "./footer.css";
+import globalStyles from "./app.css";
 
-import fbLogo from 'ionicons/dist/ionicons/svg/logo-facebook.svg';
-import twitterLogo from 'ionicons/dist/ionicons/svg/logo-twitter.svg';
-import githubLogo from 'ionicons/dist/ionicons/svg/logo-github.svg';
+import fbLogo from "ionicons/dist/ionicons/svg/logo-facebook.svg";
+import twitterLogo from "ionicons/dist/ionicons/svg/logo-twitter.svg";
+import githubLogo from "ionicons/dist/ionicons/svg/logo-github.svg";
 
 function Icon({ src, title }) {
   return (
-    <figure title={title} alt={title} dangerouslySetInnerHTML={{ __html: src }} className={styles.icon} />
+    <figure
+      title={title}
+      alt={title}
+      dangerouslySetInnerHTML={{ __html: src }}
+      className={styles.icon}
+    />
   );
 }
 
 export function Footer() {
   return (
     <footer className={classNames(styles.footer, globalStyles.padded)}>
-      <div>Songs up to date as of 5/30/2019. <a href="#" onClick={showCredits}>Credits</a></div>
       <div>
-        <a href="https://m.me/noah.manneschmidt" target="_blank"><Icon src={fbLogo} title="Contact me on Facebook Messenger" /></a>
+        Songs up to date as of 8/15/2019.{" "}
+        <a href="#" onClick={showCredits}>
+          Credits
+        </a>
+      </div>
+      <div>
+        <a href="https://m.me/noah.manneschmidt" target="_blank">
+          <Icon src={fbLogo} title="Contact me on Facebook Messenger" />
+        </a>
         &nbsp;
-        <a href="https://twitter.com/Cathadan" target="_blank"><Icon src={twitterLogo} title="Contact me on Twitter" /></a>
+        <a href="https://twitter.com/Cathadan" target="_blank">
+          <Icon src={twitterLogo} title="Contact me on Twitter" />
+        </a>
         &nbsp;
-        <a href="https://github.com/noahm/DDRCardDraw"><Icon src={githubLogo} title="Contribute on Github" /></a>
+        <a href="https://github.com/noahm/DDRCardDraw">
+          <Icon src={githubLogo} title="Contribute on Github" />
+        </a>
       </div>
     </footer>
   );
@@ -29,8 +45,10 @@ export function Footer() {
 
 function showCredits(e) {
   e.preventDefault();
-  alert('App originally by Jeff Lloyd.\
-  Weighted distribution code by Chris Chike.\
-  Jacket images prepared by FuriousDCSL.\
-  Maintained by Noah Manneschmidt.');
+  alert(
+    "App originally by Jeff Lloyd.\
+Weighted distribution code by Chris Chike.\
+Jacket images prepared by FuriousDCSL.\
+Maintained by Noah Manneschmidt."
+  );
 }
