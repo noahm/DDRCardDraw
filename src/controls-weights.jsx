6c97753f62965a8e2a23d864bc4c19fb61d3791f
Noah Manneschmidt <noah@manneschmidt.net>
2019-10-29 20:20:13 -0700

    Upgrade to Preact X

--- src/controls-weights.jsx
+++ src/controls-weights.jsx
@@ -1,27 +1,30 @@
-import { Component } from 'preact';
-import styles from './controls-weights.css';
-import { times } from './utils';
-import { Text, Localizer } from 'preact-i18n';
+import { Component } from "preact";
+import styles from "./controls-weights.css";
+import { times } from "./utils";
+import { Text, Localizer } from "preact-i18n";
 
 export class WeightsControls extends Component {
   state = {
-    weights: this.getWeightsFor(this.props),
+    weights: this.getWeightsFor(this.props)
   };
 
-  getWeightsFor(props, state = this.state) {
+  getWeightsFor(props, state = this.state || {}) {
     if (!state.weights) {
       state.weights = [];
-    };
-    return times(props.high - props.low + 1, (n) => ({
+    }
+    return times(props.high - props.low + 1, n => ({
       label: n + props.low - 1,
-      value: state.weights[n - 1] ? state.weights[n - 1].value : 1,
+      value: state.weights[n - 1] ? state.weights[n - 1].value : 1
     }));
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.low !== this.props.low || nextProps.high !== this.props.high) {
+    if (
+      nextProps.low !== this.props.low ||
+      nextProps.high !== this.props.high
+    ) {
       this.setState({
-        weights: this.getWeightsFor(nextProps),
+        weights: this.getWeightsFor(nextProps)
       });
     }
   }
@@ -32,17 +35,23 @@ export class WeightsControls extends Component {
   }
 
   render() {
-    const {
-      hidden,
-    } = this.props;
-    const totalWeight = this.state.weights.reduce((total, weight) => total + weight.value, 0);
+    const { hidden } = this.props;
+    const totalWeight = this.state.weights.reduce(
+      (total, weight) => total + weight.value,
+      0
+    );
     const percentages = this.state.weights.map(weight => {
-      return weight.value ? (100 * weight.value / totalWeight).toFixed(0) : 0;
+      return weight.value ? ((100 * weight.value) / totalWeight).toFixed(0) : 0;
     });
 
     return (
       <section className={hidden ? styles.hidden : styles.weights}>
-        <p><Text id="weights.explanation">Integers only. Set a fixed probability that charts of each difficulty level will be drawn.</Text></p>
+        <p>
+          <Text id="weights.explanation">
+            Integers only. Set a fixed probability that charts of each
+            difficulty level will be drawn.
+          </Text>
+        </p>
         {this.state.weights.map((weight, i) => {
           return (
             <label key={weight.label}>
@@ -51,22 +60,23 @@ export class WeightsControls extends Component {
                 name={`weight-${weight.label}`}
                 value={weight.value}
                 min="0"
-                onChange={(e) => this.setWeight(i, +e.currentTarget.value)}
+                onChange={e => this.setWeight(i, +e.currentTarget.value)}
               />
               {weight.label} <sub>{percentages[i]}%</sub>
             </label>
           );
         })}
         <Localizer>
-          <label title={<Text id="weights.check.title">"Prevents very unlikely outcomes (e.g. multiple 17s drawn when at 10% chance)"</Text>}>
-            <input
-              type="checkbox"
-              name="limitOutliers"
-              defaultChecked={true}
-            />
-            <Text id="weights.check.label">
-              Force Expected Distribution
-            </Text>
+          <label
+            title={
+              <Text id="weights.check.title">
+                Prevents very unlikely outcomes (e.g. multiple 17s drawn when at
+                10% chance)
+              </Text>
+            }
+          >
+            <input type="checkbox" name="limitOutliers" defaultChecked={true} />
+            <Text id="weights.check.label">Force Expected Distribution</Text>
           </label>
         </Localizer>
       </section>
