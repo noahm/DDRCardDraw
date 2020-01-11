1ade7a3f48a514fe6ee56bcd4218209d7076fff9
Noah Manneschmidt <nmannesc@twitch.tv>
2020-01-11 10:12:18 -0800

    Update validation, remove beginner from extreme

--- scripts/validate.js
+++ scripts/validate.js
@@ -6,6 +6,76 @@ const dataFileNames = readdirSync(resolve(join(__dirname, "../src/songs")));
 const songsSchema = require("../songs.schema.json");
 const schemaLocation = "src/models/SongData.ts";
 
+function validateContents(dataFile) {
+  const errors = [];
+
+  const allKeys = [
+    ...dataFile.meta.styles,
+    ...dataFile.meta.difficulties.map(d => d.key),
+    ...dataFile.meta.flags
+  ];
+  const styles = new Set(dataFile.meta.styles);
+  const difficulties = new Set(dataFile.meta.difficulties.map(d => d.key));
+  const flags = new Set(dataFile.meta.flags);
+
+  if (dataFile.meta.lvlMax < 1) {
+    errors.push("max level is below 1");
+  }
+
+  if (dataFile.defaults.style && !styles.has(dataFile.defaults.style)) {
+    errors.push("default style is not listed in meta");
+  }
+
+  if (dataFile.defaults.difficulties.some(d => !difficulties.has(d))) {
+    errors.push("some default difficulties are missing from meta");
+  }
+
+  if (dataFile.defaults.flags.some(d => !flags.has(d))) {
+    errors.push("some default flags are missing from meta");
+  }
+
+  if (dataFile.defaults.lowerLvlBound > dataFile.defaults.upperLvlBound) {
+    errors.push("default level bounds are reversed");
+  }
+
+  if (
+    dataFile.defaults.lowerLvlBound > dataFile.meta.lvlMax ||
+    dataFile.defaults.upperLvlBound > dataFile.meta.lvlMax
+  ) {
+    errors.push("default level bounds are beyond max level");
+  }
+
+  for (const key of allKeys) {
+    if (!(dataFile.i18n.en[key] && dataFile.i18n.ja[key])) {
+      errors.push("missing translation for " + key);
+    }
+    if (
+      difficulties.has(key) &&
+      !(dataFile.i18n.en["$abbr"][key] && dataFile.i18n.ja["$abbr"][key])
+    ) {
+      errors.push("missing abbreviated translation for " + key);
+    }
+  }
+
+  for (const song of dataFile.songs) {
+    for (const chart of song.charts) {
+      if (!styles.has(chart.style)) {
+        errors.push(`unrecognized style "${chart.style}" used by ${song.name}`);
+      }
+      if (!difficulties.has(chart.diffClass)) {
+        errors.push(
+          `unrecognized diffClass "${chart.diffClass}" used by ${song.name}`
+        );
+      }
+      if (chart.lvl > dataFile.meta.lvlMax) {
+        errors.push(`${song.name} has chart above level max`);
+      }
+    }
+  }
+
+  return errors;
+}
+
 let hasError = false;
 for (const dataFile of dataFileNames) {
   const songData = require(`../src/songs/${dataFile}`);
@@ -14,7 +84,14 @@ for (const dataFile of dataFileNames) {
   });
 
   if (result.valid) {
-    console.log(`${dataFile} looks good!`);
+    const consistencyErrors = validateContents(songData);
+    if (consistencyErrors.length) {
+      consistencyErrors.forEach(err => console.error(err));
+      console.log(`${dataFile} has inconsistent data!`);
+      hasError = true;
+    } else {
+      console.log(`${dataFile} looks good!`);
+    }
   } else {
     result.errors.forEach(error => {
       console.error(error.toString());
