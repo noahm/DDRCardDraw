9a1c5ee45c26ccdf1a8959fd3685078adf2328b5
Noah Manneschmidt <psoplayer@gmail.com>
2020-02-17 12:05:55 -0800

    Comment difficulty color

--- src/hooks/useDifficultyColor.ts
+++ src/hooks/useDifficultyColor.ts
@@ -1,6 +1,9 @@
 import { useContext } from "preact/hooks";
 import { DrawStateContext } from "../draw-state";
 
+/**
+ * Returns the CSS color specified in the game file metadata for a given difficulty
+ */
 export function useDifficultyColor(difficultyClass: string): string {
   const { gameData } = useContext(DrawStateContext);
   const diffAccentColor =
