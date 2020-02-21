6fd5f66e62d0c5a48df813fa5fd743ee6cdbaf4f
Noah Manneschmidt <nmannesc@twitch.tv>
2020-02-20 22:17:49 -0800

    Update footer date

--- src/footer.tsx
+++ src/footer.tsx
@@ -9,7 +9,7 @@ import { TranslateContext } from "@denysvuika/preact-translate";
 import { About } from "./about";
 
 // note that month is zero-indexed for date constructor :)
-const lastUpdate = new Date(2019, 9, 22);
+const lastUpdate = new Date(2019, 1, 20);
 
 export function Footer() {
   const { t } = useContext(TranslateContext);
