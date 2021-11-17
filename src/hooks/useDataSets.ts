73e66b86937773416caa4f7670ea49d95866e10b
Noah Manneschmidt <noah@manneschmidt.net>
2021-11-16 20:49:11 -0800

    Drop old Ace and A20 data

--- src/hooks/useDataSets.ts
+++ src/hooks/useDataSets.ts
@@ -6,6 +6,8 @@ export const available = process.env.DATA_FILES as unknown as Array<{
   display: string;
 }>;
 
+available.sort((a, b) => (a.display < b.display ? -1 : 1));
+
 export function useDataSets() {
   const { dataSetName, loadGameData } = useContext(DrawStateContext);
   const current = available.find((s) => s.name === dataSetName) || available[0];
