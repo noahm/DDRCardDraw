0cf39452b1c2f2e9f1fc6ebfd8a3cb12b12b47bd
Noah Manneschmidt <noah@manneschmidt.net>
2023-01-27 15:21:52 -0800

    remove webpack, add favicons integration to vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,43 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import fs from "fs";
 import { basename, resolve } from "path";
+import faviconsPlugin from "@darkobits/vite-plugin-favicons";
+
+const iconSource = "./public/ddr-tools-256.png";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    faviconsPlugin({
+      appName: "DDR Tools - card draw and more!",
+      appShortName: "DDR Tools",
+      theme_color: "#28b6ea",
+      display: "standalone",
+      scope: "/",
+      start_url: "/",
+      manifestMaskable: true,
+      icons: {
+        android: {
+          source: iconSource,
+          offset: 15,
+        },
+        appleIcon: {
+          source: iconSource,
+          offset: 10,
+        },
+        favicons: {
+          source: iconSource,
+        },
+        appleStartup: {
+          source: iconSource,
+        },
+      },
+    }),
+    VitePWA({
+      manifest: false,
+    }),
+  ],
   define: {
     __DATA_FILES__: JSON.stringify(
       fs.readdirSync(resolve(__dirname, "src/songs")).map((file) => ({
