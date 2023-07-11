e1bf3fe1eca6b767331c3ede7e864aa192a96975
Noah Manneschmidt <noah@manneschmidt.net>
2023-07-11 13:46:56 -0700

    remove blur, theme front page

--- src/drawing-list.tsx
+++ src/drawing-list.tsx
@@ -2,9 +2,8 @@ import { lazy, memo, useDeferredValue } from "react";
 import styles from "./drawing-list.css";
 import { useDrawState } from "./draw-state";
 import { useConfigState } from "./config-state";
-import { Callout, NonIdealState } from "@blueprintjs/core";
-import { IconNames } from "@blueprintjs/icons";
-import logo from "./assets/ddr-tools-256.png";
+import { NonIdealState } from "@blueprintjs/core";
+import logo from "./assets/last-card-socal.png";
 
 const EligibleChartsList = lazy(() => import("./eligible-charts"));
 const DrawnSet = lazy(() => import("./drawn-set"));
@@ -32,14 +31,12 @@ export function DrawingList() {
     return (
       <div className={styles.empty}>
         <NonIdealState
-          icon={<img src={logo} height={128} />}
-          title="DDR Tools"
-          description="Click 'Draw' above to draw some songs at random. Chose from other games in the top left menu."
-          action={
-            <Callout intent="primary" icon={IconNames.ThirdParty}>
-              Networking features now available! Start with the new tab in
-              settings.
-            </Callout>
+          icon={<img src={logo} height={350} />}
+          title="Last Card"
+          description={
+            <>
+              Presented by <i>SoCal DDR</i> and <i>Red Note</i>
+            </>
           }
         />
       </div>
