11f4c67e5538012cd25a722b2fa62aa936981557
Noah Manneschmidt <noah@manneschmidt.net>
2024-10-02 19:00:37 -0700

    persist to supabase in addition to partykit

--- src/party/server.ts
+++ src/party/server.ts
@@ -2,11 +2,27 @@ import type * as Party from "partykit/server";
 import type { ReduxAction, Roomstate } from "./types";
 import { configureStore } from "@reduxjs/toolkit";
 import { reducer } from "../state/root-reducer";
-import { AppState } from "../state/store";
+import { AppState, type store as appReduxStore } from "../state/store";
+
+import { createClient } from "@supabase/supabase-js";
+import { Database, Json } from "./database.types";
+
+const supabase = createClient<Database>(
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_KEY as string,
+  { auth: { persistSession: false } },
+);
+
+function isAppState(state: unknown): state is AppState {
+  if (state && !Array.isArray(state) && typeof state === "object") {
+    return "config" in state && "drawings" in state;
+  }
+  return false;
+}
 
 export default class Server implements Party.Server {
   // @ts-expect-error I assign this for sure
-  private store: ReturnType<typeof configureStore>;
+  private store: typeof appReduxStore;
 
   constructor(readonly room: Party.Room) {
     console.log("constructor start");
@@ -14,11 +30,28 @@ export default class Server implements Party.Server {
 
   async onStart() {
     const preloadedState =
-      await this.room.storage.get<AppState>("currentState");
-    this.store = configureStore({ reducer, preloadedState });
-    this.store.subscribe(() => {
-      this.room.storage.put("currentState", this.store.getState());
-    });
+      (await this.getFromStorage()) || (await this.getFromSupabase());
+    if (preloadedState) {
+      this.store = configureStore({ reducer, preloadedState });
+    } else {
+      this.store = configureStore({ reducer });
+    }
+  }
+
+  private async getFromSupabase() {
+    const { data, error } = await supabase
+      .from("event_state")
+      .select("state")
+      .eq("id", this.room.id)
+      .maybeSingle();
+    if (error) {
+      throw new Error(error.message);
+    }
+    if (data && isAppState(data.state)) return data.state;
+  }
+
+  private getFromStorage() {
+    return this.room.storage.get<AppState>("currentState");
   }
 
   onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
@@ -35,9 +68,7 @@ export default class Server implements Party.Server {
   }
 
   onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
+    // broadcast it to all the other connections in the room...
     this.room.broadcast(
       message,
       // ...except for the connection it came from
@@ -45,7 +76,17 @@ export default class Server implements Party.Server {
     );
 
     const parsed = JSON.parse(message) as ReduxAction;
+    // resolve the new state
     this.store.dispatch(parsed.action);
+    const nextState = this.store.getState();
+    // persist to partykit storage
+    this.room.storage.put("currentState", nextState);
+    // persist the state to supabase
+    supabase.from("event_state").upsert({
+      id: this.room.id,
+      state: nextState as unknown as Json,
+      updated_at: new Date().toISOString(),
+    });
   }
 
   private getRoomState() {
