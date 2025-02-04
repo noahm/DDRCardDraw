73bf44c4e7994e12d8f4ab046d252207c42ca679
Noah Manneschmidt <noah@manneschmidt.net>
2025-02-04 14:30:53 -0800

    smarter lazy-load for date filter

---
+++ src/controls/release-date-filter.tsx
@@ -0,0 +1,43 @@
+import { FormGroup } from "@blueprintjs/core";
+import { DateInput3 } from "@blueprintjs/datetime2";
+import parse from "date-fns/parse";
+import format from "date-fns/format";
+import { detectedLanguage } from "../utils";
+import { useIntl } from "../hooks/useIntl";
+import { useConfigState } from "../config-state";
+
+const dateFormat = "yyyy-MM-dd";
+export default function ReleaseDateFilterControl(props: {
+  mostRecentRelease: string;
+}) {
+  const { t } = useIntl();
+  const updateState = useConfigState((s) => s.update);
+  const cutoffDate = useConfigState((s) => s.cutoffDate);
+
+  const reference = new Date();
+  const maxDate = parse(props.mostRecentRelease, dateFormat, reference);
+  const minDate = parse("2000-01-01", dateFormat, reference);
+
+  return (
+    <FormGroup label={t("controls.releaseHeader")}>
+      <DateInput3
+        dateFnsFormat={dateFormat}
+        locale={detectedLanguage}
+        value={cutoffDate || props.mostRecentRelease}
+        onChange={(newDate, isUserChange) => {
+          if (!isUserChange) {
+            return;
+          }
+          if (!newDate) {
+            updateState({ cutoffDate: "" });
+            return;
+          }
+          updateState({ cutoffDate: format(new Date(newDate), dateFormat) });
+        }}
+        placeholder={t("controls.releaseInputPlaceholder", { dateFormat })}
+        maxDate={maxDate}
+        minDate={minDate}
+      />
+    </FormGroup>
+  );
+}
