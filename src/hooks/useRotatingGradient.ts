6f37636fc7d24d578cf89d1c5e2b22e9c0300dd0
Noah Manneschmidt <noah@manneschmidt.net>
2025-06-22 14:31:14 -0700

    improve rotating gradients with oklch

--- src/hooks/useRotatingGradient.ts
+++ src/hooks/useRotatingGradient.ts
@@ -1,11 +1,12 @@
 import { useState, type CSSProperties } from "react";
+import "./useRotatingGradient.css";
 
 const HUE_STEP = (255 / 8) * 3;
 let hue = Math.floor(Math.random() * 255);
 
 function getRandomGradiant() {
   hue += HUE_STEP;
-  return `linear-gradient(hsl(${hue}, var(--drawing-grad-saturation), var(--drawing-grad-lightness)), transparent, transparent)`;
+  return `linear-gradient(oklch(var(--drawing-grad-lightness) var(--drawing-grad-chroma) ${hue}), transparent)`;
 }
 
 export function useRotatingGradientStyles(): CSSProperties {
