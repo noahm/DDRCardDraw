f88027a8771fcc58f24fc622041c4deeaddaebcd
Nogic <24802730+nogic1008@users.noreply.github.com>
2025-08-29 15:55:36 +0900

    feat: impl SanbaiSongImporter & use on script

--- scripts/import-sanbai.mjs
+++ scripts/import-sanbai.mjs
@@ -3,124 +3,23 @@ import {
   getJacketFromRemySong,
   getMetaFromRemy,
 } from "./scraping/remy.mjs";
+import { SanbaiSongImporter } from "./scraping/sanbai.mjs";
 import { DDR_WORLD as MIX_META } from "./scraping/ddr-sources.mjs";
-import * as path from "path";
+import path from "path";
 import { readFile } from "fs/promises";
 import { fileURLToPath } from "url";
 import {
   writeJsonData,
   requestQueue,
-  reportQueueStatusLive,
   checkJacketExists,
   sortSongs,
   setJacketPrefix,
+  downloadJacket,
 } from "./utils.mjs";
 
-const idxMap = [
-  "single:beginner",
-  "single:basic",
-  "single:difficult",
-  "single:expert",
-  "single:challenge",
-  "double:basic",
-  "double:difficult",
-  "double:expert",
-  "double:challenge",
-];
-
 setJacketPrefix(MIX_META.jacketPrefix);
-
-/**
- * Invalid data on 3icecream site
- * @typedef {typeof import('./scraping/songdata.mjs').ALL_SONG_DATA[number]} SongData
- * @type {Map<SongData['song_id'], Partial<SongData>>}
- */
-const invalidDataOnSanbai = new Map([
-  [
-    "oQ0bqIQ8DdPlilO000DQloOo6Od8IdQ6", // Bloody Tears (IIDX EDITION)
-    { ratings: [4, 5, 6, 11, 0, 5, 7, 11, 0] },
-  ],
-  [
-    "61QQi8i9Iliq66IOq1ib888b666o08O8", // Mermaid girl
-    { ratings: [3, 4, 7, 11, 12, 5, 8, 11, 12] },
-  ],
-  [
-    "PddldblI909IqI8PPiQIo9lIIiQdDo1l", // MEGALOVANIA
-    { ratings: [3, 9, 12, 16, 18, 9, 12, 16, 18] },
-  ],
-  [
-    "668Q8qQdqoIQQiIQOilDqd8lDOOQ8bDQ", // パ→ピ→プ→Yeah!
-    { lock_types: [0, 0, 0, 0, 280, 0, 0, 0, 280] },
-  ],
-  [
-    "D9lq0DioIl9D6ll0d61990DP9qPPb1dP", // True Blue
-    { lock_types: [0, 0, 0, 0, 280, 0, 0, 0, 280] },
-  ],
-  [
-    "o1dbD61Qi98O60liQQ91d8O16I86dqDd", // In The Breeze
-    { lock_types: [0, 0, 0, 0, 280, 0, 0, 0, 280] },
-  ],
-  [
-    "q6iD8idqo69qIPl00IQidoq1o1o1d1b9", // クリムゾンゲイト
-    { lock_types: [0, 0, 0, 0, 280, 0, 0, 0, 280] },
-  ],
-  [
-    "qo6l9dbb8D0iDQI1odo0Qb9O1ibDdoIb", // Remain
-    { lock_types: [0, 0, 0, 0, 280, 0, 0, 0, 280] },
-  ],
-]);
-
-const lockFlags = {
-  20: ["goldExclusive", "tempUnlock"], // BEMANI PRO LEAGUE -SEASON 4- Triple Tribe
-  190: ["grandPrixPack"], // DDR GRAND PRIX packs
-  240: ["unlock"], // BEMANI PRO LEAGUE -SEASON 5- Triple Tribe 0 (2025-07-17 10:00~2025-08-31 23:59)
-  250: ["flareRank"], // FLARE SKILL unlock
-  260: ["tempUnlock"], // MYSTICAL Re:UNION
-  270: ["worldLeague"], // WORLD LEAGUE
-  280: ["unlock"], // EXTRA SAVIOR WORLD
-  290: ["unlock"], // GALAXY BRAVE
-  300: ["platinumMembers"], // DDR PLATINUM MEMBERS
-};
-
 const manualyAddFlags = new Set(["copyStrikes"]);
 
-/**
- *
- * @param {string} diffClass
- * @param {string} style
- * @returns
- */
-function diffIdxFor(diffClass, style) {
-  return idxMap.indexOf(`${style}:${diffClass}`);
-}
-
-/**
- *
- * @param {number} idx
- * @returns {[style: string, diffClass: string]}
- */
-function styleAndDiffFor(idx) {
-  const ret = idxMap[idx].split(":");
-  return [ret[0], ret[1]];
-}
-
-/**
- *
- * @param {string} id
- * @param {string} diffClass
- * @param {string} style
- * @returns
- */
-function chartKeyFor(id, diffClass, style) {
-  return `${id}/${diffIdxFor(diffClass, style)}`;
-}
-import {
-  ALL_SONG_DATA as sanbaiData,
-  SONG_DATA_LAST_UPDATED_unixms,
-} from "./scraping/songdata.mjs";
-
-const ui = reportQueueStatusLive();
-let warnings = 0;
 try {
   const targetFile = path.join(
     path.dirname(fileURLToPath(import.meta.url)),
@@ -135,89 +34,20 @@ try {
     await readFile(targetFile, { encoding: "utf-8" }),
   );
 
-  const foldersByIndex = existingData.meta.folders.slice();
-  foldersByIndex.reverse();
-  foldersByIndex.unshift("zero");
+  // Fetch 3icecream data using SanbaiSongImporter
+  const importer = new SanbaiSongImporter();
+  const sanbaiSongs = await importer.fetchSongs();
 
-  // const log = (...msgs) =>
-  //   ui.log.write(msgs.map((item) => item.toString()).join(" "));
-
-  ///** @type {Record<string, ReturnType<typeof getDifficultyList>>} */
-  // const ratingsLists = {};
-
-  // /**
-  //  * @param {number} difficulty
-  //  * @param {string} style
-  //  */
-  // function getCachedDifficultyList(difficulty, style) {
-  //   const key = `${style}:${difficulty}`;
-  //   if (!ratingsLists[key]) {
-  //     ratingsLists[key] = requestQueue.add(
-  //       () => getDifficultyList(difficulty, style),
-  //       {
-  //         throwOnTimeout: true,
-  //       },
-  //     );
-  //   }
-  //   return ratingsLists[key];
-  // }
-
-  // let warnings = 0;
-  // const ui = reportQueueStatusLive();
-  // for (const song of existingData.songs) {
-  //   for (const chart of song.charts) {
-  //     getCachedDifficultyList(chart.lvl, chart.style).then(
-  //       (difficultyRatings) => {
-  //         const chartKey = chartKeyFor(song.saHash, chart.diffClass, chart.style);
-  //         const maybeRating = difficultyRatings[chartKey];
-  //         if (!maybeRating) {
-  //           ui.log.write(`No tier for ${song.name} - ${chartKey}`);
-  //           warnings++;
-  //         } else {
-  //           chart.sanbaiTier = chart.lvl + maybeRating.tier;
-  //         }
-  //       },
-  //     );
-  //   }
-  // }
-  const tasks = sanbaiData.map(async (song) => {
-    // Fix invalid data
-    if (invalidDataOnSanbai.has(song.song_id)) {
-      const actual = invalidDataOnSanbai.get(song.song_id);
-      for (const [key, value] of Object.entries(actual)) {
-        if (!Array.isArray(value) && value !== song[key]) {
-          ui.log.write(
-            `Fixing invalid ${key} of ${song.song_name} on 3ice : ${song[key]} -> ${value}`,
-          );
-          song[key] = value;
-        } else if (
-          Array.isArray(value) &&
-          (value.length !== song[key]?.length ||
-            value.some((v, i) => v !== song[key][i]))
-        ) {
-          ui.log.write(
-            `Fixing invalid ${key} of ${song.song_name} on 3ice : ${song[key]} -> ${value}`,
-          );
-          song[key] = value;
-          if (key === "ratings") {
-            song.tiers = song.tiers.map((_) => 1); // reset tiers
-          }
-        } else {
-          ui.log.write(
-            `invalidDataOnSanbai has ${song.song_name}.${key}, but no change needed`,
-          );
-        }
-      }
-    }
-
+  // Merge with existing data
+  const tasks = sanbaiSongs.map(async (sanbaiSong) => {
     const existingSong = existingData.songs.find(
-      (s) => s.saHash === song.song_id || s.name === song.song_name,
+      (s) => s.saHash === sanbaiSong.saHash || s.name === sanbaiSong.name,
     );
 
     // Delete songs that are removed from the game
-    if (song.deleted) {
+    if (sanbaiSong.deleted) {
       if (existingSong) {
-        ui.log.write(`Deleting removed song: ${existingSong.name}`);
+        console.log(`Deleting removed song: ${existingSong.name}`);
         existingData.songs = existingData.songs.filter(
           (s) => s !== existingSong,
         );
@@ -225,194 +55,164 @@ try {
       return;
     }
 
-    const locks = song.lock_types;
-    /** @type {number|null} */
-    let songLock = null;
-    if (locks) {
-      const allChartsLocked = song.ratings.every(
-        (lvl, idx) => !lvl || locks[idx],
-      );
-      if (allChartsLocked) {
-        songLock = locks[0];
-      }
-    }
-
-    const charts = song.ratings
-      .map((lvl, idx) => {
-        const [style, diffClass] = styleAndDiffFor(idx);
-        /** @type {import('../src/models/SongData.js').Chart} */
-        const chart = {
-          lvl,
-          style,
-          diffClass,
-        };
-        if (locks && locks[idx] && locks[idx] !== songLock) {
-          chart.flags = lockFlags[locks[idx]];
-        }
-        if (song.tiers[idx] && song.tiers[idx] !== 1) {
-          chart.sanbaiTier = chart.lvl + song.tiers[idx];
-        }
-        return chart;
-      })
-      .filter((c) => !!c.lvl);
-
     if (existingSong) {
       // add missing id
       if (!existingSong.saHash) {
-        existingSong.saHash = song.song_id;
+        existingSong.saHash = sanbaiSong.saHash;
       }
       // update charts/flags
-      for (const freshChartData of charts) {
+      for (const chart of sanbaiSong.charts) {
         const existingChart = existingSong.charts.find(
-          (existingChart) =>
-            existingChart.diffClass === freshChartData.diffClass &&
-            existingChart.style === freshChartData.style,
+          (c) => c.style === chart.style && c.diffClass === chart.diffClass,
         );
         if (!existingChart) {
-          ui.log.write(
-            `Adding missing ${freshChartData.diffClass} chart of ${song.song_name}`,
+          console.log(
+            `Adding missing ${chart.diffClass} chart of ${sanbaiSong.name}`,
           );
-          existingSong.charts.push(freshChartData);
+          existingSong.charts.push(chart);
           continue;
         }
-
-        const givenTier =
-          song.tiers[diffIdxFor(existingChart.diffClass, existingChart.style)];
-        if (givenTier && givenTier !== 1) {
-          existingChart.sanbaiTier = freshChartData.lvl + givenTier;
-        }
-        if (existingChart.lvl !== freshChartData.lvl) {
-          ui.log.write(
-            `Updating lvl of ${song.song_name} ${freshChartData.diffClass}`,
+        if (existingChart.lvl !== chart.lvl) {
+          console.log(
+            `Updating lvl of ${sanbaiSong.name} ${chart.diffClass}: ${existingChart.lvl} -> ${chart.lvl}`,
           );
-          existingChart.lvl = freshChartData.lvl;
+          existingChart.lvl = chart.lvl;
+        }
+        if (chart.sanbaiTier && chart.sanbaiTier !== existingChart.sanbaiTier) {
+          existingChart.sanbaiTier = chart.sanbaiTier;
         }
 
         // Update chart flags
         const meaningfulChartFlags = (existingChart.flags ?? []).filter(
           (f) => !manualyAddFlags.has(f),
         );
-        freshChartData.flags = [
+        chart.flags = [
           ...(existingChart.shock ? ["shock"] : []),
-          ...(freshChartData.flags ?? []),
+          ...(chart.flags ?? []),
         ];
         if (
-          meaningfulChartFlags.length !== freshChartData.flags.length ||
-          meaningfulChartFlags.some((f, i) => f !== freshChartData.flags[i])
+          meaningfulChartFlags.length !== chart.flags.length ||
+          meaningfulChartFlags.some((f, i) => f !== chart.flags[i])
         ) {
-          ui.log.write(
-            `Updating flags of ${song.song_name} ${freshChartData.diffClass}`,
+          console.log(
+            `Updating flags of ${sanbaiSong.name} ${chart.diffClass}`,
           );
           existingChart.flags = [
             ...(existingChart.flags?.filter((f) => manualyAddFlags.has(f)) ??
               []),
-            ...(freshChartData.flags ?? []),
+            ...(chart.flags ?? []),
           ];
           if (!existingChart.flags.length) {
             delete existingChart.flags;
           }
         }
       }
+
       // Update song flags
       const meaningfulSongFlags = (existingSong.flags ?? []).filter(
         (f) => !manualyAddFlags.has(f),
       );
       if (
-        meaningfulSongFlags.length !== (lockFlags[songLock]?.length ?? 0) ||
-        meaningfulSongFlags.some((f, i) => f !== lockFlags[songLock][i])
+        meaningfulSongFlags.length !== (sanbaiSong.flags?.length ?? 0) ||
+        meaningfulSongFlags.some((f, i) => f !== sanbaiSong.flags[i])
       ) {
-        ui.log.write(
+        console.log(
           `Updating flags [${existingSong.flags}] from ${existingSong.name}`,
         );
         existingSong.flags = [
           ...(existingSong.flags?.filter((f) => manualyAddFlags.has(f)) ?? []),
-          ...(lockFlags[songLock] ?? []),
+          ...(sanbaiSong.flags ?? []),
         ];
         if (!existingSong.flags.length) {
           delete existingSong.flags;
         }
       }
 
+      // Fill remyLink/jacket/bpm/artist if missing
       if (!existingSong.remyLink) {
-        const remyLink = await guessUrlFromName(song.song_name);
-        ui.log.write("guessed url as " + (remyLink || "null"));
+        const remyLink = await guessUrlFromName(sanbaiSong.name);
+        console.log("guessed url as " + (remyLink || "null"));
         existingSong.remyLink = remyLink || null;
       }
       if (!existingSong.jacket) {
         if (existingSong.remyLink) {
-          ui.log.write(
+          console.log(
             `missing jacket for ${existingSong.name}, fetching from remy`,
           );
           existingSong.jacket = await getJacketFromRemySong(
             existingSong.remyLink,
             existingSong.name,
           );
-          ui.log.write(`updated jacket property to ${existingSong.jacket}`);
-        } else {
-          ui.log.write(
-            `no remy link for ${existingSong.name}, so will remain without image`,
+        }
+        // Download jacket from 3icecream if still missing
+        if (!existingSong.jacket) {
+          console.log(
+            `missing jacket for ${existingSong.name}, fetching from 3icecream`,
+          );
+          existingSong.jacket = downloadJacket(
+            importer.getJacketUrl(sanbaiSong),
+            sanbaiSong.name,
           );
         }
+        console.log(`updated jacket property to ${existingSong.jacket}`);
       }
       if (existingSong.artist === "???" || existingSong.bpm === "???") {
-        if (!existingSong.remyLink) {
-          ui.log.write(
-            `missing remy link for ${existingSong.name} so can't add bpm or artist data`,
-          );
+        if (existingSong.remyLink) {
+          const meta = await getMetaFromRemy(existingSong.remyLink);
+          existingSong.artist = meta.artist || existingSong.artist;
+          existingSong.bpm = meta.bpm || existingSong.bpm;
         }
-        const meta = await getMetaFromRemy(existingSong.remyLink);
-        existingSong.artist = meta.artist || "???";
-        existingSong.bpm = meta.bpm || "???";
       }
     } else {
       // insert new song (need to find jacket, bpm, folder, etc)
-      ui.log.write(`Adding new song: ${song.song_name}`);
-      const remyLink = await guessUrlFromName(song.song_name);
-      ui.log.write("guessed url as " + (remyLink || "null"));
-      let jacket = checkJacketExists(song.song_name);
+      console.log(`Adding new song: ${sanbaiSong.name}`);
+      const remyLink = await guessUrlFromName(sanbaiSong.name);
+      console.log("guessed url as " + (remyLink || "null"));
+      let jacket = checkJacketExists(sanbaiSong.name);
       if (remyLink && !jacket) {
-        jacket = await getJacketFromRemySong(remyLink, song.song_name);
+        jacket = await getJacketFromRemySong(remyLink, sanbaiSong.name);
+      }
+      if (!jacket) {
+        jacket = downloadJacket(
+          importer.getJacketUrl(sanbaiSong),
+          sanbaiSong.name,
+        );
       }
       const meta = remyLink
         ? await getMetaFromRemy(remyLink)
         : { artist: null, bpm: null };
       existingData.songs.push({
-        name: song.song_name,
-        name_translation: song.romanized_name,
-        artist: meta.artist || "???",
-        folder: foldersByIndex[song.version_num],
-        bpm: meta.bpm || "???",
-        charts,
-        flags:
-          songLock && lockFlags[songLock] ? lockFlags[songLock] : undefined,
+        name: sanbaiSong.name,
+        name_translation: sanbaiSong.name_translation,
+        artist: meta.artist || sanbaiSong.artist || "???",
+        bpm: meta.bpm || sanbaiSong.bpm || "???",
+        charts: sanbaiSong.charts,
+        flags: sanbaiSong.flags,
         jacket,
-        remyLink: remyLink ? remyLink : undefined,
-        saHash: song.song_id,
-        search_hint:
-          [song.searchable_name, song.alternate_name]
-            .filter(Boolean)
-            .join(" ") || undefined,
+        remyLink,
+        saHash: sanbaiSong.saHash,
+        search_hint: sanbaiSong.search_hint,
       });
     }
   });
 
   await Promise.all(tasks);
   await requestQueue.onIdle();
-  await new Promise((resolve) => {
-    setTimeout(resolve, 500);
-  });
 
   existingData.songs = sortSongs(existingData.songs);
 
-  await writeJsonData(existingData, targetFile, SONG_DATA_LAST_UPDATED_unixms);
-  if (warnings) {
-    ui.log.write(`Done, with ${warnings} warnings`);
-  } else {
-    ui.log.write("Done");
-  }
-  ui.close();
+  const { SONG_DATA_LAST_UPDATED_unixms: lastUpdated } = await import(
+    "./scraping/songdata.mjs"
+  );
+  await writeJsonData(existingData, targetFile, lastUpdated);
+
+  console.log(`Successfully updated ${MIX_META.filename}`);
+  console.log(`Total songs in database: ${existingData.songs.length}`);
+  console.log(
+    `Songs from 3icecream: ${sanbaiSongs.filter((s) => !s.deleted).length}`,
+  );
+  console.log("Done");
 } catch (e) {
-  ui.close();
-  console.error(e);
+  console.error("Error updating sanbai data:", e);
   process.exitCode = 1;
 }
--- scripts/scraping/sanbai.mjs
+++ scripts/scraping/sanbai.mjs
@@ -1,45 +1,268 @@
+// @ts-check
+import { writeFile } from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import { requestQueue } from "../utils.mjs";
+import { format } from "prettier";
+
+/** @typedef {import("../../src/models/SongData.ts").Song} Song */
+/** @typedef {import("../../src/models/SongData.ts").Chart} Chart */
+/** @typedef {typeof import('./songdata.mjs').ALL_SONG_DATA[number]} SanbaiSong */
+
 /**
- *
- * @param {number} difficulty
- * @param {string} style
- * @returns {Promise<Record<string, { tier: number, rating: number }>>}
+ * Mapping from 3icecream's `lock_types` to DDRCardDraw's `flags`
+ * @type {Record<number, Song['flags']>}
  */
-export async function getDifficultyList(difficulty, style) {
-  let url = `https://3icecream.com/difficulty_list/${difficulty}`;
-  if (style === "double") {
-    url += "?spdp=1";
-  }
-  const resp = await fetch(url);
-  const body = await resp.text();
-  const difficultyListMatch = body.match(/let difficultyList = (\{.+\});/);
-  if (difficultyListMatch) {
-    return JSON.parse(difficultyListMatch[1]);
-  }
-}
+const lockFlags = {
+  20: ["goldExclusive", "tempUnlock"], // BEMANI PRO LEAGUE -SEASON 4- Triple Tribe
+  190: ["grandPrixPack"], // DDR GRAND PRIX packs
+  240: ["unlock"], // BEMANI PRO LEAGUE -SEASON 5- Triple Tribe 0 (2025-07-17 10:00~2025-08-31 23:59)
+  250: ["flareRank"], // FLARE SKILL unlock
+  260: ["tempUnlock"], // MYSTICAL Re:UNION
+  270: ["worldLeague"], // WORLD LEAGUE
+  280: ["unlock"], // EXTRA SAVIOR WORLD
+  290: ["unlock"], // GALAXY BRAVE
+  300: ["platinumMembers"], // DDR PLATINUM MEMBERS
+};
 
-const titleList = [
-  { name: "DanceDanceRevolution World" },
-  { name: "DanceDanceRevolution A3" },
-  { name: "DanceDanceRevolution A20 PLUS" },
-  { name: "DanceDanceRevolution A20" },
-  { name: "DanceDanceRevolution A" },
-  { name: "DanceDanceRevolution (2014)" },
-  { name: "DanceDanceRevolution (2013)" },
-  { name: "DanceDanceRevolution X3 vs 2nd MIX" },
-  { name: "DanceDanceRevolution X2" },
-  { name: "DanceDanceRevolution X" },
-  { name: "DanceDanceRevolution SuperNOVA2" },
-  { name: "DanceDanceRevolution SuperNOVA" },
-  { name: "DanceDanceRevolution EXTREME" },
-  { name: "DDRMAX2 -DanceDanceRevolution 7thMIX-" },
-  { name: "DDRMAX -DanceDanceRevolution 6thMIX-" },
-  { name: "DanceDanceRevolution 5th Mix" },
-  { name: "DanceDanceRevolution 4th Mix" },
-  { name: "DanceDanceRevolution 3rd Mix" },
-  { name: "DanceDanceRevolution 2nd Mix" },
-  { name: "DanceDanceRevolution 1st Mix" },
-].toReversed();
+/**
+ * Mapping from 3icecream's `version_num` to DDR folder name
+ * @type {Record<SanbaiSong['version_num'], Song['folder']>}
+ */
+const titleList = {
+  1: "DanceDanceRevolution 1st Mix",
+  2: "DanceDanceRevolution 2nd Mix",
+  3: "DanceDanceRevolution 3rd Mix",
+  4: "DanceDanceRevolution 4th Mix",
+  5: "DanceDanceRevolution 5th Mix",
+  6: "DDRMAX -DanceDanceRevolution 6thMIX-",
+  7: "DDRMAX2 -DanceDanceRevolution 7thMIX-",
+  8: "DanceDanceRevolution EXTREME",
+  9: "DanceDanceRevolution SuperNOVA",
+  10: "DanceDanceRevolution SuperNOVA2",
+  11: "DanceDanceRevolution X",
+  12: "DanceDanceRevolution X2",
+  13: "DanceDanceRevolution X3 vs 2nd MIX",
+  14: "DanceDanceRevolution (2013)",
+  15: "DanceDanceRevolution (2014)",
+  16: "DanceDanceRevolution A",
+  17: "DanceDanceRevolution A20",
+  18: "DanceDanceRevolution A20 PLUS",
+  19: "DanceDanceRevolution A3",
+  20: "DanceDanceRevolution World",
+};
 
-export async function getSanbaiData() {
-  return require("./songdata.mjs");
+/**
+ * Correction map for invalid data on 3icecream site
+ * @type {Map<SanbaiSong['song_id'], Partial<SanbaiSong>>}
+ */
+const invalidDataOnSanbai = new Map([
+  [
+    "oQ0bqIQ8DdPlilO000DQloOo6Od8IdQ6", // Bloody Tears (IIDX EDITION)
+    { ratings: [4, 5, 6, 11, 0, 5, 7, 11, 0] },
+  ],
+  [
+    "61QQi8i9Iliq66IOq1ib888b666o08O8", // Mermaid girl
+    { ratings: [3, 4, 7, 11, 12, 5, 8, 11, 12] },
+  ],
+  [
+    "PddldblI909IqI8PPiQIo9lIIiQdDo1l", // MEGALOVANIA
+    { ratings: [3, 9, 12, 16, 18, 9, 12, 16, 18] },
+  ],
+  [
+    "668Q8qQdqoIQQiIQOilDqd8lDOOQ8bDQ", // パ→ピ→プ→Yeah!
+    { lock_types: [0, 0, 0, 0, 280, 0, 0, 0, 280] },
+  ],
+  [
+    "D9lq0DioIl9D6ll0d61990DP9qPPb1dP", // True Blue
+    { lock_types: [0, 0, 0, 0, 280, 0, 0, 0, 280] },
+  ],
+  [
+    "o1dbD61Qi98O60liQQ91d8O16I86dqDd", // In The Breeze
+    { lock_types: [0, 0, 0, 0, 280, 0, 0, 0, 280] },
+  ],
+  [
+    "q6iD8idqo69qIPl00IQidoq1o1o1d1b9", // クリムゾンゲイト
+    { lock_types: [0, 0, 0, 0, 280, 0, 0, 0, 280] },
+  ],
+  [
+    "qo6l9dbb8D0iDQI1odo0Qb9O1ibDdoIb", // Remain
+    { lock_types: [0, 0, 0, 0, 280, 0, 0, 0, 280] },
+  ],
+  [
+    "1diQi81loIodIdOlQ8Pd6Qd8b69Q1DP8", // Destiny lovers
+    { lock_types: [0, 0, 0, 0, 190, 0, 0, 0, 190] },
+  ],
+  [
+    "1PQdDiqOD6o1b61iiDOoiiblIQbI91Pb", // JET WORLD
+    { lock_types: [0, 0, 0, 0, 190, 0, 0, 0, 190] },
+  ],
+  [
+    "60QoP9DoIo90D616989Q0D0iodOoOd91", // SEXY PLANET
+    { lock_types: [0, 0, 0, 0, 190, 0, 0, 0, 190] },
+  ],
+  [
+    "9lob1d1QPd9qiPlQOQ6l0dbodOoDPq1d", // think ya better D
+    { lock_types: [0, 0, 0, 0, 190, 0, 0, 0, 190] },
+  ],
+  [
+    "i11d86DOOdOb8Pbb1QqIilQI9Idib8PP", // Music In The Rhythm
+    { lock_types: [0, 0, 0, 0, 190, 0, 0, 0, 190] },
+  ],
+  [
+    "ii6Oooool0IoOqi1qdDo96QIil6IoOq0", // BURNIN' THE FLOOR
+    { lock_types: [0, 0, 0, 0, 190, 0, 0, 0, 190] },
+  ],
+  [
+    "lO68Q0iPIOiOIDDd8dOPoiql9OI81DQ0", // The Least 100sec
+    { lock_types: [0, 0, 0, 0, 190, 0, 0, 0, 190] },
+  ],
+  [
+    "PP9QDQ0IQQID00P61d8qdDdP09b19iiI",
+    {
+      name: "Blind Justice ～Torn souls, Hurt Faiths～",
+      lock_types: [0, 0, 0, 0, 190, 0, 0, 0, 190],
+    },
+  ],
+  [
+    "qIP6DPdbD9iO86i1DO9qDd8l6dPdbl0P", // LOVE♥SHINE
+    { lock_types: [0, 0, 0, 0, 190, 0, 0, 0, 190] },
+  ],
+  [
+    "qQ9Oo611P0dObD8q6O0Q968bbl8I91OO", // TRIP MACHINE ～luv mix～
+    { lock_types: [0, 0, 0, 0, 190, 0, 0, 0, 190] },
+  ],
+]);
+
+export class SanbaiSongImporter {
+  /**
+   * Get jacket image URL from 3icecream for a song
+   * @param {Pick<Song, "saHash">} song Song info with saHash
+   */
+  getJacketUrl(song) {
+    return `https://3icecream.com/img/banners/${song.saHash}.jpg`;
+  }
+
+  /**
+   * Fetches and converts song data from 3icecream (ALL_SONG_DATA)
+   * Applies corrections and returns array for merging
+   * @returns {Promise<(Pick<Song, 'name' | 'name_translation' | 'artist' | 'bpm' | 'charts' | 'flags' | 'saHash' | 'search_hint'> & { deleted: boolean })[]>}
+   */
+  async fetchSongs() {
+    await this.updateSongDataFile();
+    const { ALL_SONG_DATA } = await import("./songdata.mjs");
+
+    const songs = [];
+    for (const song of ALL_SONG_DATA) {
+      // Fix invalid data
+      const actual = invalidDataOnSanbai.get(song.song_id);
+      if (actual) {
+        for (const [key, value] of Object.entries(actual)) {
+          if (!Array.isArray(value) && value !== song[key]) {
+            console.log(
+              `Fixing invalid ${key} for ${song.song_name} on 3ice : ${song[key]} -> ${value}`,
+            );
+            song[key] = value;
+          } else if (
+            Array.isArray(value) &&
+            (value.length !== song[key]?.length ||
+              value.some((v, i) => v !== song[key][i]))
+          ) {
+            console.log(
+              `Fixing invalid ${key} for ${song.song_name} on 3ice : ${song[key]} -> ${value}`,
+            );
+            song[key] = value;
+            if (key === "ratings" && Array.isArray(song.tiers)) {
+              song.tiers = song.tiers.map(() => 1); // reset tiers
+            }
+          } else {
+            console.warn(
+              `No changes needed for ${key} on ${song.song_name}. Consider remove on invalidDataOnSanbai map.`,
+            );
+          }
+        }
+      }
+
+      const locks = song.lock_types;
+      let songLock = null;
+      if (locks) {
+        // If all charts are locked, set songLock
+        const allChartsLocked = song.ratings.every(
+          (lvl, idx) => !lvl || locks[idx],
+        );
+        if (allChartsLocked) songLock = locks[0];
+      }
+
+      // Build charts array from ratings and lock_types
+      const ratingsToStyleAndDiff = [
+        { style: "single", diffClass: "beginner" },
+        { style: "single", diffClass: "basic" },
+        { style: "single", diffClass: "difficult" },
+        { style: "single", diffClass: "expert" },
+        { style: "single", diffClass: "challenge" },
+        { style: "double", diffClass: "basic" },
+        { style: "double", diffClass: "difficult" },
+        { style: "double", diffClass: "expert" },
+        { style: "double", diffClass: "challenge" },
+      ];
+      const charts = song.ratings
+        .map((lvl, idx) => {
+          /** @type {Chart} */
+          const chart = { lvl, ...ratingsToStyleAndDiff[idx] };
+          if (locks && locks[idx] && locks[idx] !== songLock) {
+            chart.flags = lockFlags[locks[idx]];
+          }
+          if (song.tiers[idx] && song.tiers[idx] !== 1) {
+            chart.sanbaiTier = chart.lvl + song.tiers[idx];
+          }
+          return chart;
+        })
+        .filter((c) => !!c.lvl);
+
+      // Push converted song object (include deleted property)
+      songs.push({
+        name: song.song_name,
+        name_translation: song.romanized_name,
+        artist: "???",
+        folder: titleList[song.version_num - 1],
+        bpm: "???",
+        charts,
+        flags:
+          songLock && lockFlags[songLock] ? lockFlags[songLock] : undefined,
+        saHash: song.song_id,
+        search_hint:
+          [song.searchable_name, song.alternate_name]
+            .filter(Boolean)
+            .join(" ") || undefined,
+        deleted: !!song.deleted || false,
+      });
+    }
+    return songs;
+  }
+
+  /**
+   * Updates the local songdata.mjs file by fetching from 3icecream
+   * Formats and saves as ESM
+   * @returns {Promise<string>} Path to the updated songdata.mjs file
+   * @private
+   */
+  async updateSongDataFile() {
+    const url = "https://3icecream.com/js/songdata.js";
+    const jsText = await requestQueue.add(async () => {
+      const resp = await fetch(url);
+      return await resp.text();
+    });
+    if (!jsText) return null;
+
+    const mjsText = jsText
+      .replace(/\b(const|var)\b/g, "export const")
+      .replace(/\u00a0/g, " "); // Replace non-breaking spaces
+    const filePath = path.join(
+      path.dirname(fileURLToPath(import.meta.url)),
+      "songdata.mjs",
+    );
+    const formatted = await format(mjsText, { filepath: filePath });
+    await writeFile(filePath, formatted);
+    return filePath;
+  }
 }
