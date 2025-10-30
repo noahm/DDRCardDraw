e55a8ef2d024e0788a44a097874c5b6c9853640a
Nogic <24802730+nogic1008@users.noreply.github.com>
2025-10-31 08:40:04 +0900

    feat(songs): add スペシャル楽曲パック feat.東方Project vol.7 songs
    - 4NT1 D34D
    - 東方妖々夢 ULTIMATE MEDLEY
    - SkyDrive! (HASEKO EUROBEAT MIX)
    - Deadly Dolly Dance

--- scripts/scraping/sanbai.mjs
+++ scripts/scraping/sanbai.mjs
@@ -57,24 +57,36 @@ const titleList = {
  * @type {Map<SanbaiSong['song_id'], Partial<SanbaiSong>>}
  */
 const invalidDataOnSanbai = new Map([
+  // #region DDR GRAND PRIX スペシャル楽曲パック feat.東方Project vol.7
   [
-    "DDlbDddPO8liioQ196P0I9090bDDPQ6Q", // 迷宮のロンド
-    { lock_types: [290, 290, 290, 290, 290, 290, 290, 290, 290] },
-  ],
-  // #region EXTRA SAVIOR WORLD (BEMANI PRO LEAGUE -SEASON 4- Triple Tribe)
-  [
-    "68oQQ8lO1qbi0qI8lbO60bP1098qOD88", // COSMIC V3LOCITY
-    { lock_types: [280, 280, 280, 280, 0, 280, 280, 280, 0] },
-  ],
-  [
-    "Q9iiOo800blDqO0DDDPI1PIbl1lIidI1", // 混乱少女♥そふらんちゃん!!
-    { lock_types: [280, 280, 280, 280, 280, 280, 280, 280, 280] },
+    "08QQ8iPldb6o96bl0ld61qb99Q0Q19Qd", // 4NT1 D34D
+    {
+      ratings: [2, 5, 11, 15, 0, 5, 10, 15, 0],
+      lock_types: [190, 190, 190, 190, 0, 190, 190, 190, 0],
+    },
   ],
   [
-    "DOd9bIqddQ91qbQI1bdbbDi1lqPObo8l", // ハイテックトキオ
-    { lock_types: [280, 280, 280, 280, 280, 280, 280, 280, 280] },
+    "6IDll6i6d8iII686qq6DddiI00iD8o9d", // 東方妖々夢 ULTIMATE MEDLEY
+    {
+      ratings: [5, 10, 12, 16, 18, 10, 12, 16, 18],
+      lock_types: [190, 190, 190, 190, 190, 190, 190, 190, 190],
+    },
   ],
-  // #endregion EXTRA SAVIOR WORLD (BEMANI PRO LEAGUE -SEASON 4- Triple Tribe)
+  [
+    "bPI6QobOQl9Q161l8l68069QdO1QbO9b", // SkyDrive! (HASEKO EUROBEAT MIX)
+    {
+      ratings: [3, 7, 12, 14, 17, 8, 12, 14, 17],
+      lock_types: [190, 190, 190, 190, 190, 190, 190, 190, 190],
+    },
+  ],
+  [
+    "D90qii9D6P0POQ69Iiblib00Po1DIoi8", // Deadly Dolly Dance
+    {
+      ratings: [2, 5, 12, 17, 0, 5, 11, 17, 0],
+      lock_types: [190, 190, 190, 190, 0, 190, 190, 190, 0],
+    },
+  ],
+  // #endregion DDR GRAND PRIX スペシャル楽曲パック feat.東方Project vol.7
 ]);
 
 export class SanbaiSongImporter {
