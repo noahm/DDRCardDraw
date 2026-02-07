cc7b4ca7ad5f979d032ba2b9a88865252679a72a
chizu <59990434+Acretion@users.noreply.github.com>
2026-02-06 23:23:38 -0600

    Update maimai-patches.mjs

--- scripts/maimai/maimai-patches.mjs
+++ scripts/maimai/maimai-patches.mjs
@@ -2,85 +2,84 @@
  * @file maimai-patch.mjs
  * Fields here will override/fill missing song info or chart info
  */
- // Song Patch Example
-   // "エンジェル ドリーム" : {
-      // title: "test",
-      // artist: "test",
-      // bpm: 100,
-      // imageName: "testjacket.png",
-      // version: "test",                  -> Must match existing folder values exactly or folder filter will not work with song
-      // charts: [
-        // {
-	      // index: 0,                     -> Adjust value to change which Chart to patch
-          // style: "single",
-          // diffClass: "master",          -> "basic, advanced, expert, master, remaster, utage"
-          // lvl: 1,                       -> Levels 1.0 - 15.0
-          // extras: "std"                 -> std | dx
-        // }
-     // ]
-   // }
-		
+// Song Patch Example
+// "エンジェル ドリーム" : {
+// title: "test",
+// artist: "test",
+// bpm: 100,
+// imageName: "testjacket.png",
+// version: "test",                  -> Must match existing folder values exactly or folder filter will not work with song
+// charts: [
+// {
+// index: 0,                     -> Adjust value to change which Chart to patch
+// style: "single",
+// diffClass: "master",          -> "basic, advanced, expert, master, remaster, utage"
+// lvl: 1,                       -> Levels 1.0 - 15.0
+// extras: "std"                 -> std | dx
+// }
+// ]
+// }
+
 // https://silentblue.remywiki.com/
 export const MAIMAI_PATCH = {
-  "ぽっぴっぽー": {
+  ぽっぴっぽー: {
     artist: "Lamaze P",
   },
-  "ダブルラリアット": {
+  ダブルラリアット: {
     bpm: 138,
   },
-  "洗脳": {
+  洗脳: {
     bpm: 125,
   },
   "チュルリラ・チュルリラ・ダッダッダ！": {
     bpm: 220,
   },
-  "エイリアンエイリアン": {
+  エイリアンエイリアン: {
     bpm: 152,
   },
-  "Seyana. ～何でも言うことを聞いてくれるアカネチャン～" : {
-	bpm: 144,
+  "Seyana. ～何でも言うことを聞いてくれるアカネチャン～": {
+    bpm: 144,
   },
-  "マイオドレ！舞舞タイム" : {
-	bpm: 183,
+  "マイオドレ！舞舞タイム": {
+    bpm: 183,
   },
-  "テレキャスタービーボーイ" : {
-	bpm: 182,
+  テレキャスタービーボーイ: {
+    bpm: 182,
   },
-  "アンビバレンス" : {
-	bpm: 187,
+  アンビバレンス: {
+    bpm: 187,
   },
-  "ラグトレイン" : {
-	bpm: 147,
+  ラグトレイン: {
+    bpm: 147,
   },
-  "アカツキアライヴァル" : {
-	bpm: 125,
+  アカツキアライヴァル: {
+    bpm: 125,
   },
-  "ヒトガタ" : {
-	bpm: 150,
+  ヒトガタ: {
+    bpm: 150,
   },
-  "スカーレット警察のゲットーパトロール24時" : {
-	bpm: 160,
+  スカーレット警察のゲットーパトロール24時: {
+    bpm: 160,
   },
-  "さくゆいたいそう" : {
-	bpm: 160,
+  さくゆいたいそう: {
+    bpm: 160,
   },
-  "のじゃロリック" : {
-	bpm: 160,
+  のじゃロリック: {
+    bpm: 160,
   },
-  "匿名M" : {
-	bpm: 140,
+  匿名M: {
+    bpm: 140,
   },
-  "Ultimate taste" : {
-	bpm: 150,
+  "Ultimate taste": {
+    bpm: 150,
   },
-  "メズマライザー" : {
-	bpm: 185,
+  メズマライザー: {
+    bpm: 185,
   },
-  "ありきたりな恋の歌" : {
-	bpm: 180,
-  },
-  "エンジェル ドリーム" : {
+  ありきたりな恋の歌: {
     bpm: 180,
-   }
+  },
+  "エンジェル ドリーム": {
+    bpm: 180,
+  },
 };
-
