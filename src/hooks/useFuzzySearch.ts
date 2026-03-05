115fbaf96c7affa76af4862c3849047bb66cd0d5
Noah Manneschmidt <noah@manneschmidt.net>
2026-03-05 13:11:56 -0800

    memoize fuzzy search

--- src/hooks/useFuzzySearch.ts
+++ src/hooks/useFuzzySearch.ts
@@ -1,14 +1,26 @@
 import FuzzySearch from "fuzzy-search";
 import { useGameData } from "../state/hooks";
+import { useMemo } from "react";
 
 export function useFuzzySearch() {
   const gameData = useGameData();
-  if (!gameData) return null;
-  return new FuzzySearch(
-    gameData.songs,
-    ["name", "name_translation", "search_hint", "artist", "artist_translation"],
-    {
-      sort: true,
-    },
+  return useMemo(
+    () =>
+      !gameData
+        ? null
+        : new FuzzySearch(
+            gameData.songs,
+            [
+              "name",
+              "name_translation",
+              "search_hint",
+              "artist",
+              "artist_translation",
+            ],
+            {
+              sort: true,
+            },
+          ),
+    [gameData],
   );
 }
