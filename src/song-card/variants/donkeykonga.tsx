d6b88e41ca0e4020f6f23d6b8db9b60929700a39
Noah Manneschmidt <noah@manneschmidt.net>
2026-04-13 16:38:47 -0700

    remove unneeded duplication from dk conga custom card

--- src/song-card/variants/donkeykonga.tsx
+++ src/song-card/variants/donkeykonga.tsx
@@ -1,40 +1,13 @@
 import React from "react";
-import {
-  EligibleChart,
-  DrawnChart,
-  PlayerPickPlaceholder,
-  CHART_PLACEHOLDER,
-} from "../../models/Drawing";
 import { detectedLanguage } from "../../utils";
 import styles from "../song-card.css";
+import { baseChartValues, CardSectionProps } from "./base";
 
 const isJapanese = detectedLanguage === "ja";
 
-/** Props passed into the main song card component to provide implementations of main visual content */
-export interface CardContentsProps {
-  CenterContent: React.ComponentType<CardSectionProps>;
-  FooterContent: React.ComponentType<CardSectionProps>;
-}
-
-/** Props passed to child components that get to customize card center and footer content */
-export interface CardSectionProps {
-  chart: EligibleChart | DrawnChart | PlayerPickPlaceholder;
-}
-
-export function donkeyKongaChartValues(
-  chart: EligibleChart | DrawnChart | PlayerPickPlaceholder,
-): Partial<EligibleChart> & { name: string } {
-  if ("type" in chart && chart.type === CHART_PLACEHOLDER) {
-    return {
-      name: "Your Pick Here",
-    };
-  }
-  return chart;
-}
-
 export function DonkeyKongaCardCenter(props: CardSectionProps) {
   const { name, nameTranslation, artist, artistTranslation, folder } =
-    donkeyKongaChartValues(props.chart);
+    baseChartValues(props.chart);
   return (
     <>
       <div className={styles.name} title={nameTranslation}>
